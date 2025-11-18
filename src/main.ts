634487a3af0c89faba3d494c6dce45ef0b2272a2
Eduardo Arruda <13435770+eduardo-arruda@users.noreply.github.com>
2025-11-18 17:10:29 -0300

    Padroniza /api/v1/docs

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/v1/docs', app, document);
 
   // Prefixo global de API (excluindo /health)
   app.setGlobalPrefix('api/v1', {
